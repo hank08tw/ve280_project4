990dafd7355645e01a7b09fe606d1329d7262bda
hank08tw <hank08tw@sjtu.edu.cn>
2018-07-10 21:32:31 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/ve280_project4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ve280_project4.dir/p4.cpp.o"
+  "ve280_project4.pdb"
+  "ve280_project4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ve280_project4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
